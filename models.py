919761397fe506028b1621b1d5a9b26230ad3f68
Kiru <kirushanth.ravindran@gmail.com>
2021-04-28 13:31:11 -0700

    init commit

---
+++ models.py
@@ -0,0 +1,38 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String(50),
+                    nullable=False)               
+    last_name = db.Column(db.String(50),
+                    nullable=False)
+    image_url = db.Column(db.String(), nullable=True, default='https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=963&q=80')
+    
+    #ask about creating uniqueness for full profile?
+
+    def __repr__(self):
+        """Show info about user."""
+
+        u = self
+        return f"<User {u.id} {u.first_name} {u.last_name}>"
+
+    @classmethod
+    def get_all_users():
+        return cls.query.all()
+        
